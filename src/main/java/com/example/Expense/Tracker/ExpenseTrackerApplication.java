51e0c99fd8608e61747c910368ea1ec1a8397e3b
akashsekar6292 <akashsekanna@gmail.com>
2025-11-16 00:02:04 +0530

    02 application started by Thomas

--- src/main/java/com/example/Expense/Tracker/ExpenseTrackerApplication.java
+++ src/main/java/com/example/Expense/Tracker/ExpenseTrackerApplication.java
@@ -3,11 +3,14 @@ package com.example.Expense.Tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.Clock;
+
 @SpringBootApplication
 public class ExpenseTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseTrackerApplication.class, args);
+        System.out.println("Application started by Thomas");
 	}
 
 }
